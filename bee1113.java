e26c26761c2da891592fc0bbca346bc48d680aea
Lucas Romero <lucasromero.ag@outlook.com>
2025-07-17 22:16:19 +0100

    Estrutura repetitiva (enquanto - while)

--- bee1113.java
+++ bee1113.java
@@ -5,7 +5,8 @@ public class bee1113 {
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
-		
+
+		//Código melhorado e mais limpo após verificar a aula de resolução do Professor Nélio!
 		int num1 = sc.nextInt();
 		int num2 = sc.nextInt();
 		
@@ -14,14 +15,12 @@ public class bee1113 {
 			if(num1 > num2)
 			{
 				System.out.println("Decrescente");
-				num1 = sc.nextInt();
-				num2 = sc.nextInt();
 			}
 			else {
 				System.out.println("Crescente");
-				num1 = sc.nextInt();
-				num2 = sc.nextInt();
 			}
+			num1 = sc.nextInt();
+			num2 = sc.nextInt();
 		}
 		
 		sc.close();
