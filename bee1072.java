622dd20014b6de3efae0fd706ee50214a2cd17b1
Lucas Romero <lucasromero.ag@outlook.com>
2025-07-23 11:00:08 +0100

    Estrutura repetitiva (para - for)

---
+++ bee1072.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+
+public class bee1072 {
+
+	public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+
+	int n = sc.nextInt();
+	
+	int x = 0;
+	
+	int in = 0;
+	int out = 0;
+	for(int i=0; i<n; i++)
+	{
+		x = sc.nextInt();
+		if(x>=10 && x<=20)
+		{
+			in+=1;
+		}
+		else
+		{
+			out+=1;
+		}
+	}
+	
+	System.out.println(in + " in");
+	System.out.println(out + " out");
+		
+	sc.close();
+	}
+	
+}
+
